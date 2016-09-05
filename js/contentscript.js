0d5aa2318eebba700e632e47fcbe17d165468733
Rounak Banik <rounakbanik@gmail.com>
2016-09-06 00:24:15 +0530

    Add Online Courses and Practice links.

--- js/contentscript.js
+++ js/contentscript.js
@@ -23,11 +23,6 @@ function HTMLParse(event) {
 		if(xhr.readyState == 4) {
 			info = xhr.responseText;
 			info_obj = jQuery.parseHTML(info);
-			 /*$(".img-container img").fadeOut(400, function() {
-            		$(this).attr('src',img_src);
-            		$(".img-container img").css("transform", event.scale);
-					$(".img-container img").css("margin-top", event.margin);
-        		}).fadeIn(400);*/
 			console.log(xhr);
 			content = "<h3 class='text-center'><a href='https://news.ycombinator.com/'>News</a></h3><ol>";
 			$(info_obj).find(".storylink").each(function(index) {
@@ -36,8 +31,6 @@ function HTMLParse(event) {
 				content = content + "<li><a href='" + link + "'>" + title + "</a></li>";
 				return index < 7;
 			});
-			//console.log(content);
-			//$(".news ol").append(content);
 			content = content + "</ol><br><div class='other text-center'><a href='http://www.theverge.com/''><img src='images/verge.ico'></a>&nbsp;<a href='http://mashable.com/'><img src='images/mashable.png'></a>&nbsp;<a href='http://slashdot.org/'><img src='images/slashdot.png'></a>&nbsp;<a href='https://techcrunch.com/'><img src='images/techcrunch.ico'></a></div>";
 			$(".news").fadeOut(400, function() {
 				$(".news").css("background-color", "#eee");
